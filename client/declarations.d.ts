31a50a706b13659969ebc13a94514ed577949cc8
Demidog03 <olzhasktl362@gmail.com>
2023-09-06 08:55:10 +0600

    disabling eslint 5

--- client/declarations.d.ts
+++ client/declarations.d.ts
@@ -1,11 +1,13 @@
-// declare module '*.scss' {
-//   const content: Record<string, string>;
-//   export default content;
-// }
-// declare module "*.png";
-// declare module "*.jpg";
-// // declare module "*.jpeg";
-// declare module "*.svg";
-// declare module "*.gif";
-// declare module "@reduxjs/*"
-// declare module "@*"
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.scss";
+declare module "*.module.scss";
+declare module "@reduxjs/*"
+declare module "@*"
